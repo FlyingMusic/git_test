63a72171fa006eba4a0cc66a36ee11d3073c52fb
FlyingMusic <1243050373@qq.com>
2021-06-28 19:23:33 +0800

    添加 EM 自己推导实例

--- python/mytest/em/selfDeduction.py
+++ python/mytest/em/selfDeduction.py
@@ -68,19 +68,75 @@ def calcEM(height):
         times += 1 
     return now
 
+def myEM(height):
+    N = len(height)
+    # 假设两个分布的参数已知
+    gmu, gsigma = 150, 10
+    bmu, bsigma = 200, 10
+
+    for i in range(100):
+        # 计算参数下的权重分配
+        gweight = []
+        bweight = []
+        for h in height:
+            gw = stats.norm.pdf(h, gmu, gsigma)
+            bw = stats.norm.pdf(h, bmu, bsigma)
+            gweight.append(gw / (gw + bw))
+            bweight.append(bw / (gw + bw))
+        #print(gweight)
+        #print(bweight)
+
+        # 用新权重更新参数
+        wsum = 0.0
+        gmuSum = 0.0 
+        for h,w in zip(height, gweight):
+            gmuSum += h * w
+            wsum += w
+
+        gmu = gmuSum / wsum
+       # print("gmu = %f\n" % gmu)
+
+        gsigmaSum = 0.0
+        for h,w in zip(height, gweight):
+            gsigmaSum += (h - gmu) * (h - gmu) * w
+        gsigma = gsigmaSum / wsum
+        gsigma = math.sqrt(gsigma)
+       # print("gsigma = %f\n" % gsigma)
+
+
+        wsum = 0.0
+        bmuSum = 0.0 
+        for h,w in zip(height, bweight):
+            bmuSum += h * w
+            wsum += w
+
+        bmu = bmuSum / wsum
+       # print("bmu = %f\n" % bmu)
+
+        bsigmaSum = 0.0
+        for h,w in zip(height, bweight):
+            bsigmaSum += (h - bmu) * (h - bmu) * w
+        bsigma = bsigmaSum / wsum
+        bsigma = math.sqrt(bsigma)
+       # print("bsigma = %f\n" % bsigma)
+
+        print("gmu[%f] gsigma[%f] bmu[%f] bsigma[%f]\n" % (gmu, gsigma, bmu, bsigma))
+
 if __name__=='__main__':
     #模拟产生男女身高各100个
-    male_array = np.random.normal(loc = 200, scale = 5.0, size = 100)
-    fmale_array = np.random.normal(loc = 100, scale = 5.0, size = 100)
-    print(male_array)
-    print(fmale_array)
+    male_array = np.random.normal(loc = 165, scale = 5.0, size = 1000)
+    fmale_array = np.random.normal(loc = 175, scale = 5.0, size = 1000)
+    #print(male_array)
+    #print(fmale_array)
 
     #混合数据
     merged_array = []
     for i in range(len(male_array)):
         merged_array.append(male_array[i])
         merged_array.append(fmale_array[i])
+   # print(merged_array)
+    merged_array.sort()
     print(merged_array)
 
-    calcEM(merged_array)    
+    myEM(merged_array)    
 
